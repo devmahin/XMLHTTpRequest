d7ffecd6e6fa0a526a941b4efb700d04cebd283d
devmahin <mahinhowlader123@gmail.com>
2023-12-11 22:17:05 +0600

    yield

--- ES6 GENARATOR/script.js
+++ ES6 GENARATOR/script.js
@@ -9,15 +9,18 @@
 
 ////////////========genaretor
 function *genaretor(){
-    yield 1,
-    yield 2,
-    yield 3,
-    yield 4,
-    yield 5,
-    yield 6
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+    yield 6;
+    return "Done";
 }
 let gen = genaretor()
 
+console.log(gen.next())
+console.log(gen.next())
 console.log(gen.next())
 console.log(gen.next())
 console.log(gen.next())
